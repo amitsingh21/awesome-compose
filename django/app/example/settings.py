7485541372e415ff6ee87c785a68993cc8470857
Amit  Singh <amitsingh21991@gmail.com>
2022-11-10 14:02:02 +0530

    used riomterics middleware

--- django/app/example/settings.py
+++ django/app/example/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware.iometrics.HTTPMetricsMiddleware',
+    'rio_metrics.middleware.HTTPMetricsMiddleware',
 ]
 
 ROOT_URLCONF = 'example.urls'
